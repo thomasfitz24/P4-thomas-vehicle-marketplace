a74427565ed44a05feb2f8d5755cecb45387f519
thomasfitz24 <thomasfitz2412@gmail.com>
2026-01-03 21:51:14 +0000

    Display product images on product list and detail pages

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- products/models.py
+++ products/models.py
@@ -27,6 +27,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     year = models.PositiveIntegerField()
     mileage = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
