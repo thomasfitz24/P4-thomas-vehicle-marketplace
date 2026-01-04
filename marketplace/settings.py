1007a4258a7f11ac88e3896e5518b81e9085ebb3
thomasfitz24 <thomasfitz2412@gmail.com>
2026-01-04 23:19:34 +0000

    Update settings and ignore local database file

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -57,6 +57,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "marketplace.wsgi.application"
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
