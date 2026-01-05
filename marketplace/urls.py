b9b0b589a7bb4f5c28c65636d88d2ee7de4d02b8
thomasfitz24 <thomasfitz2412@gmail.com>
2026-01-05 20:35:09 +0000

    Fix auth template folder typo (registration) to prevent 500 error

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("products.urls")),
     path("checkout/", include("checkout.urls")),
 ]
