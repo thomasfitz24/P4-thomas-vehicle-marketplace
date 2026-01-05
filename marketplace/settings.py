a3971c475718fe8ba073b78781f91eb9f71f6a9a
thomasfitz24 <thomasfitz2412@gmail.com>
2026-01-05 23:42:53 +0000

    Heroku is so annoying

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -118,7 +118,8 @@ LOGOUT_REDIRECT_URL = "/"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 
-os.environ.setdefault(
-    "CLOUDINARY_URL",
-    "CLOUDINARY_URL=cloudinary://<397568769876832>:<C40qTxWIZvZMt9lh4uEP03sVF3w>@dlkst7boo",
-)
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": "dlkst7boo",
+    "API_KEY": "397568769876832",
+    "API_SECRET": "C40qTxWIZvZMt9lh4uEP03sVF3w",
+}
