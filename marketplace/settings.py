04ef8aab6fe4f3de4a3326a6fd48d833d32b2366
thomasfitz24 <thomasfitz2412@gmail.com>
2026-01-05 21:10:04 +0000

    Switch to safer static files storage

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -104,7 +104,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
