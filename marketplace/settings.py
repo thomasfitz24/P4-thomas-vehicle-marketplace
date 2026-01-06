309099e69007dad0b2f152aae946d5af4bdd89cd
thomasfitz24 <thomasfitz2412@gmail.com>
2026-01-06 12:33:22 +0000

    Configure WhiteNoise for static files on Heroku

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -112,7 +112,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
