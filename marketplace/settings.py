14d791e5c86995c9604c470e19b7cd56fdbd9689
thomasfitz24 <thomasfitz2412@gmail.com>
2026-01-06 18:29:58 +0000

    Fix WhiteNoise collectstatic admin SVG error

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -92,6 +92,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+WHITENOISE_MANIFEST_STRICT = False
+
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
