4b897281e0ab79e0a295d99bb1fddb9f0289cc24
thomasfitz24 <thomasfitz2412@gmail.com>
2026-01-06 19:59:47 +0000

    Fix static files configuration

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -90,7 +90,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
